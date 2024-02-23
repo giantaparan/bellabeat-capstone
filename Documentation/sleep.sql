94363d090345838dc7cf7887d093dbcb500709ab
Gian Taparan <gtaparan00@gmail.com>
2024-02-22 21:39:40 -0800

    updated sleep.sql

--- Documentation/sleep.sql
+++ Documentation/sleep.sql
@@ -53,3 +53,49 @@ SELECT
 from `fitbit_users.sleepDay`;
 # check that no entries were lost when cleaning
 # both show 173240
+
+SELECT
+  Id, PARSE_TIMESTAMP('%m/%d/%Y %I:%M:%S %p', date) as date, value, logId
+from `fitbit_users.minuteSleep`;
+# converts date from type string to timestamp
+# saved as minuteSleep_clean
+
+SELECT
+  Id, sleep_start AS sleep_date, COUNT(logId) AS number_naps, SUM(EXTRACT(HOUR
+FROM
+time_sleeping)) AS total_time_sleeping
+FROM (
+  SELECT
+    Id, logId, MIN(DATE(date)) AS sleep_start, MAX(DATE(date)) AS sleep_end,
+TIME( TIMESTAMP_DIFF(MAX(date),MIN(date),HOUR),
+MOD(TIMESTAMP_DIFF(MAX(date),MIN(date),MINUTE),60),
+MOD(MOD(TIMESTAMP_DIFF(MAX(date),MIN(date),SECOND),3600),60) ) AS time_sleeping
+FROM `fitbit_users.minuteSleep_clean`
+WHERE value=1
+GROUP BY 1, 2)
+WHERE sleep_start=sleep_end
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+# shows number, length, and date of naps by each user
+# saved as naps_by_user
+# assumes naps are anytime someone sleeps but wakes up on the same day
+
+SELECT
+  Id, sum(number_naps) as total_naps, sum(total_time_sleeping) as agg_time_sleeping
+from `fitbit_users.naps_by_user`
+group by Id
+order by Id ASC;
+# shows number of naps per user and amount of time spent napping
+# saved as naps_by_user_agg
+
+SELECT *
+from `fitbit_users.naps_by_user_agg`
+inner join `fitbit_users.dailyActivity_by_user`
+on `fitbit_users.naps_by_user_agg`.Id=`fitbit_users.dailyActivity_by_user`.Id;
+# shows total naps and total active minutes by user
+# saved as napsVsActivity
+
+SELECT * EXCEPT(Id_1), total_VAM+total_FAM+total_LAM as total_active_minutes
+from `fitbit_users.napsVsActivity`
+order by Id ASC;
+# saved by napsVsActivity_clean
